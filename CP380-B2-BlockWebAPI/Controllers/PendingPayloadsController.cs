e0d5bd6c68e228391e7b233e950593004776c6c8
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-08-14 11:58:01 -0400

    Update PendingPayloadsController.cs

--- CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
+++ CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
@@ -1,8 +1,7 @@
-﻿using CP380_B2_BlockWebAPI.Models;
 using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +9,29 @@ using System.Threading.Tasks;
 
 namespace CP380_B2_BlockWebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     [Produces("application/json")]
     public class PendingPayloadsController : ControllerBase
     {
-        public object DependencyManager { get; private set; }
-
-        [HttpGet("/latest")]
-        public string Getblocks()
+        private PendingPayloads pendingPayload;
+        public PendingPayloadsController(PendingPayloads pendingPayloads)
         {
-            BlockList blockList = new BlockList();
-            var blocks = blockList.Chain;
-            return JsonConvert.SerializeObject(blocks);
+            pendingPayload = pendingPayloads;
         }
 
-        [HttpPost("/add")]
-        public void AddNext(int index)
+         [HttpGet("/latest")]
+        public IActionResult Get()
         {
-            BlockList blist = new BlockList();
-            var data = HttpContext.Request.ReadFromJsonAsync<Payload>();
-#pragma warning disable CS8073 // The result of the expression is always the same since a value of this type is never equal to 'null'
-            if (data != null && data.Result != null)
-#pragma warning restore CS8073 // The result of the expression is always the same since a value of this type is never equal to 'null'
-                blist.AddNext(data.Result, index);
+            return Ok(pendingPayload.Payloads);
+        }
+
+         [HttpPost("/add")]
+        public IActionResult Post(Payload payload)
+        {
+            pendingPayload.Payloads.Add(payload);
+            return Ok();
         }
     }
 }
+
